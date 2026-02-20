ff5a25aacdb90a10214f49a9027ebc1371a59486
Maher Wahid <mahernama410@gmail.com>
2026-02-20 14:53:04 -0600

    Update App() for Landing Page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage";
 
 function App() {
     return (
         <div className="App">
-            <h1>Welcome to the clinic</h1>
-
-            <nav>
-                <Link to="/user">Go for user here</Link> |{" "}
-                <Link to="/clinic">Go for clinic here</Link>
-            </nav>
-
             <Routes>
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/user" element={<h2>User login details here</h2>} />
                 <Route path="/clinic" element={<h2>Clinic login here</h2>} />
             </Routes>
