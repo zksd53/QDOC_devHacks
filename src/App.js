3324c818983a15c50c0ec3fb31325e2877a11f92
Maher Wahid <mahernama410@gmail.com>
2026-02-20 14:36:58 -0600

    Add Routing for User and Clinic logins

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Welcome to the clinic</h1>
+
+            <nav>
+                <Link to="/user">Go for user here</Link> |{" "}
+                <Link to="/clinic">Go for clinic here</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/user" element={<h2>User login details here</h2>} />
+                <Route path="/clinic" element={<h2>Clinic login here</h2>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
