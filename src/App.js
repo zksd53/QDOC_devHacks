5a9ac9ac33de842b768d8f1f80d454f4792b0d7e
Maher Wahid <mahernama410@gmail.com>
2026-02-20 15:35:45 -0600

    Add routing for login patient

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route, Link, useLocation } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import FirstTimeUser from "./user/first_time";
 import PatientDashboard from "./user/dashboard/PatientDashboard";
+import PatientLogin from "./user/PatientLogin";
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/patient" element={<FirstTimeUser />} />
+                <Route path="/patient-signup" element={<FirstTimeUser />} />
+                <Route path="/patient-login" element={<PatientLogin />} />
                 <Route path="/clinic" element={<h2>Clinic login here</h2>} />
-                <Route path="/user/dashboard" element={<PatientDashboard />} />
+                <Route path="/patient/dashboard" element={<PatientDashboard />} />
             </Routes>
         </div>
     );
