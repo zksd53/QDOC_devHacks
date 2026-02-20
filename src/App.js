58d9702c2a9c8124a68b2cca6b3342ca8472ea45
Maher Wahid <mahernama410@gmail.com>
2026-02-20 16:31:26 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./home_dashBoard/LandingPage";
 import FirstTimeUser from "./user/first_time";
 import PatientDashboard from "./user/dashboard/PatientDashboard";
 import PatientLogin from "./user/PatientLogin";
+import ClinicLogin from "./client/ClinicLogin";
 
 function App() {
     return (
@@ -12,8 +13,8 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/patient-signup" element={<FirstTimeUser />} />
                 <Route path="/patient-login" element={<PatientLogin />} />
-                <Route path="/clinic" element={<h2>Clinic login here</h2>} />
-                <Route path="/patient/dashboard" element={<PatientDashboard />} />
+                <Route path="/clinic-login" element={<ClinicLogin />} />
+                <Route path="/patient-dashboard" element={<PatientDashboard />} />
             </Routes>
         </div>
     );
---
+++ src/client/ClinicLogin.jsx
@@ -0,0 +1,8 @@
+import "../user/Login.css";
+import Login from "../user/Login";
+
+export default function ClinicLogin() {
+    return (
+        <Login userType={"Clinic"} successLink={"clinic-dashboard"} signupLink={"clinic-signup"} />
+    );
+}
